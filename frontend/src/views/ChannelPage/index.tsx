0f5a97641d5ad3161a9d77d5facc5cf327cced63
Tobias Kärst <github@tobiaskaerst.dev>
2025-01-20 17:41:15 +0100

    feat: linting errors

--- frontend/src/views/ChannelPage/index.tsx
+++ frontend/src/views/ChannelPage/index.tsx
@@ -2,7 +2,6 @@
 
 import type { FC } from 'react';
 
-import { useAuthenticationStore } from '$/lib/supabase/authentication/useAuthenticationStore';
 import { ChannelSidebar } from '$/views/ChannelPage/components/ChannelSidebar';
 import { Messages } from '$/views/ChannelPage/components/Messages';
 
@@ -10,9 +9,7 @@ interface Props {
   id: string;
 }
 
-export const ChannelPage: FC<Props> = ({ id }) => {
-  const user = useAuthenticationStore((s) => s.user);
-
+export const ChannelPage: FC<Props> = () => {
   return (
     <div className='grid h-screen grid-cols-[auto_1fr] items-center justify-center'>
       <ChannelSidebar />
