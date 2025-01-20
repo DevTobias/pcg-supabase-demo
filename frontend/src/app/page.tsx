5346b509c640cac080ee0128a628e0e47b80a42f
Tobias Kärst <github@tobiaskaerst.dev>
2025-01-20 12:08:54 +0100

    feat: added shadcn

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
-    <div className='flex h-screen items-center justify-center bg-slate-900'>
-      <h1 className='text-5xl font-bold text-neutral-100'>Supabase Demo</h1>
+    <div className='flex h-screen items-center justify-center'>
+      <h1 className='text-5xl font-bold'>Supabase Demo</h1>
     </div>
   );
 }
